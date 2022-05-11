7c41f5efb731bcc5459b08a6b6148bbf587d1655
KimrAndrew <andrewrk1997@gmail.com>
2022-05-11 14:45:11 -0700

    adds instantiation test for Customer model

--- inventory_manager/models/Customer.py
+++ inventory_manager/models/Customer.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Customer():
+class Customer(models.Model):
     first_name = models.CharField(max_length=16)
     last_name = models.CharField(max_length=16)
     address = models.CharField(max_length=256)
--- inventory_manager/tests.py
+++ inventory_manager/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from inventory_manager.models.Customer import Customer
 
 from inventory_manager.models.Inventory_Item import Inventory_Item
 from .models.Manufacturer import Manufacturer
@@ -21,15 +22,26 @@ class InventoryItemTest(TestCase):
         quantity = 600
 
         item = Inventory_Item.objects.create(
-            name=item_name,
-            description=item_description,
-            price=price,
-            quantity= quantity,
+            name='Slides',
+            description='The coolest slides in town',
+            price=500,
+            quantity= 600,
             manufacturer = manufacturer
             )
-            
+
         self.assertEqual(item.name,item_name)
         self.assertEqual(item.description, item_description)
         self.assertEqual(item.price,price)
         self.assertEqual(item.quantity,quantity)
         self.assertEqual(item.manufacturer,manufacturer)
+
+class CustomerTest(TestCase):
+    def test_instantiate_customer(self):
+        customer_first_name = 'P'
+        customer_last_name='Sherman'
+        customer_address='42 Wallaby Way, Sydney'
+        customer = Customer(first_name='P', last_name='Sherman', address='42 Wallaby Way, Sydney')
+
+        self.assertEqual(customer.first_name, customer_first_name)
+        self.assertEqual(customer.last_name, customer_last_name)
+        self.assertEqual(customer.address, customer_address)
