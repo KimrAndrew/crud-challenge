9380ebd6fe639380d60d4d505f57b43ecb9ca6d7
KimrAndrew <andrewrk1997@gmail.com>
2022-05-11 14:51:11 -0700

    adds Merchant model

--- inventory_manager/models/Manufacturer.py
+++ inventory_manager/models/Manufacturer.py
@@ -1,4 +1,3 @@
-from django.db import models
 from .Base_Entity import Base_Entity
 
 class Manufacturer(Base_Entity):
---
+++ inventory_manager/models/Merchant.py
@@ -0,0 +1,4 @@
+from .Base_Entity import Base_Entity
+
+class Merchant(Base_Entity):
+    pass
