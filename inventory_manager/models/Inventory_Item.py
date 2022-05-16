779af29dfeb79a1896312363d1e3c165c2da0900
KimrAndrew <andrewrk1997@gmail.com>
2022-05-16 16:38:05 -0700

    adds docstring to InventoryItem model

--- inventory_manager/models/Inventory_Item.py
+++ inventory_manager/models/Inventory_Item.py
@@ -1,4 +1,11 @@
 from .Base_Inventory_Item import BaseInventoryItem
 
 class InventoryItem(BaseInventoryItem):
+    """
+    Fields:
+    - name: str
+    - description: str
+    - price: int
+    - quantity: int
+    """
     pass
