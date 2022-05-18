0cebcf92c2ffffae873e3cc5dfb5c6555c41b1a7
KimrAndrew <andrewrk1997@gmail.com>
2022-05-18 15:57:12 -0700

    adds views for Warehouse and WherehouseItem models

--- warehouse_manager/views.py
+++ warehouse_manager/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
-# Create your views here.
+from .models import WarehouseInventoryItem, Warehouse
+
+class Warehouse_Item_Detail(RetrieveUpdateDestroyAPIView):
+    queryset = WarehouseInventoryItem.objects.all()
+    serializer_class=WarehouseInventoryItemSerializer
+
+class Warehouse_Item_List(ListCreateAPIView):
+    queryset = WarehouseInventoryItem.objects.all()
+    serializer_class = WarehouseInventoryItemSerializer
+
+class Warehouse_List(ListCreateAPiView):
+    queryset = Warehouse.objects.all()
+    serializer_class = WarehouseSerializer
+
+class Warehouse_detail(RetrieveUpdateDestroyAPIView):
+    queryset = Warehouse.objects.all()
+    serializer_class = WarehouseSerializer
