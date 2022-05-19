0f59f17253e22b409b3bcfca32c9dbcbb0f1920c
KimrAndrew <andrewrk1997@gmail.com>
2022-05-18 18:28:23 -0700

    fixes broken Meta classes

--- warehouse_manager/serializers.py
+++ warehouse_manager/serializers.py
@@ -2,11 +2,11 @@ from rest_framework.serializers import ModelSerializer
 from .models import WarehouseInventoryItem, Warehouse
 
 class WarehouseInventoryItemSerializer(ModelSerializer):
-    class meta:
+    class Meta:
         model = WarehouseInventoryItem
         fields = '__all__'
 
 class WarehouseSerializer(ModelSerializer):
-    class meta:
+    class Meta:
         model = Warehouse
         fields = '__all__'
