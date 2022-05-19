de8a5ca47a71eb372655e6bd3e8841ec3ce1bd7c
KimrAndrew <andrewrk1997@gmail.com>
2022-05-18 19:55:26 -0700

    adds views for warehouse_manager

--- warehouse_manager/urls.py
+++ warehouse_manager/urls.py
@@ -6,5 +6,6 @@ from .views import Warehouse_Item_Detail,Warehouse_Item_List,Warehouse_List,Ware
 urlpatterns = [
     path("<int:pk>/",Warehouse_Detail.as_view(), name='warehouse_detail'),
     path("",Warehouse_List.as_view(),name="warehouse_list"),
-    path("<int:pk>/inventory/",Warehouse_Item_List.as_view(),name='warehouse_inventory')
+    path("inventory/<int:warehouse>/",Warehouse_Item_List.as_view(),name='warehouse_inventory'),
+    path("inventory/<int:warehouse>/<int:pk>/",Warehouse_Item_Detail.as_view(),name="warehouse_item_detail")
 ]
