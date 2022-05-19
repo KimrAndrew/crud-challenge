7ce2674ccd6583452bcbd6651a5f8939bd9c4cdb
KimrAndrew <andrewrk1997@gmail.com>
2022-05-18 18:29:05 -0700

    starts custom view for Warehouse Inventory

--- warehouse_manager/views.py
+++ warehouse_manager/views.py
@@ -1,4 +1,7 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.filters import BaseFilterBackend
 
 from .models import WarehouseInventoryItem, Warehouse
 from .serializers import WarehouseInventoryItemSerializer, WarehouseSerializer
@@ -7,14 +10,18 @@ class Warehouse_Item_Detail(RetrieveUpdateDestroyAPIView):
     queryset = WarehouseInventoryItem.objects.all()
     serializer_class=WarehouseInventoryItemSerializer
 
-class Warehouse_Item_List(ListCreateAPIView):
-    queryset = WarehouseInventoryItem.objects.all()
-    serializer_class = WarehouseInventoryItemSerializer
+class Warehouse_Item_List(APIView):
+    def get(self,request,pk):
+        warehouse = Warehouse.objects.filter(id=pk)[0]
+        print(warehouse)
+        inventory_queryset = WarehouseInventoryItem.objects.filter(warehouse=warehouse)
+        serialized_inventory = WarehouseInventoryItemSerializer(inventory_queryset,many=True)
+        return Response(serialized_inventory.data)
 
 class Warehouse_List(ListCreateAPIView):
     queryset = Warehouse.objects.all()
     serializer_class = WarehouseSerializer
 
-class Warehouse_detail(RetrieveUpdateDestroyAPIView):
+class Warehouse_Detail(RetrieveUpdateDestroyAPIView):
     queryset = Warehouse.objects.all()
     serializer_class = WarehouseSerializer
